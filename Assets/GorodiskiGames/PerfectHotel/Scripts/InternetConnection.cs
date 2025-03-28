cb8ba95c8b006c7309777247379f5ecf7830a333
Amit7M <112542159+Amit7M@users.noreply.github.com>
2025-03-28 13:48:28 +0530

    added internet connection ui.

--- Assets/GorodiskiGames/PerfectHotel/Scripts/InternetConnection.cs
+++ Assets/GorodiskiGames/PerfectHotel/Scripts/InternetConnection.cs
@@ -1,23 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InternetConnection : MonoBehaviour
 {
 
+    [SerializeField] private GameObject connectionLost;
+    [SerializeField] private Button connectionButton;
+
     void Start()
     {
+        InternetConnectionCheck();
+    }
+
+    private void Update()
+    {
+        if (Time.timeScale == 0)
+        {
+            Debug.Log("Trigger this function : ");
+            connectionButton.onClick.AddListener(InternetConnectionCheck);
+        }
+    }
+
+    public void InternetConnectionCheck()
+    {
+        Debug.Log("Error. InternetConnectionCheck : start");
+
         if (Application.internetReachability == NetworkReachability.NotReachable)
         {
-            Debug.Log("Error. Check internet connection!");
+            Debug.Log("Error. Check internet connection! : not working");
+            connectionLost.SetActive(true);
+            Time.timeScale = 0;
+
         }
 
         else
         {
             Debug.Log("Error. Check internet connection! : working");
+            connectionLost.SetActive(false);
+            Time.timeScale = 1;
         }
-
     }
 
-    
 }
